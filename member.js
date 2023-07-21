8ed7e3e9b5143ffd55eea10fc3fe5b14beb41638
doczekal <44091376+doczekal@users.noreply.github.com>
2023-07-21 18:03:27 +0000

    second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = {
+    name: 'John Doe',
+    age: 30,
+    address: '123 Main St',
+    skills: ['JavaScript', 'HTML', 'CSS']
+  };
+  console.log(member.skills);
+  return member;
+}
